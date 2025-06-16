81c4f968384375328f31e122ea29bb64d4dd2932
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-06-16 20:14:41 +0200

    Add code in app.services.__init__ file, add 2 test files, add code given by Holberton to the app.api.v1.amenities file

--- part2/hbnb/app/services/facade.py
+++ part2/hbnb/app/services/facade.py
@@ -1,4 +1,9 @@
 from app.persistence.repository import InMemoryRepository
+from app.models.amenity import Amenity
+from app.models.place import Place
+from app.models.user import User
+from app.models.review import Review
+
 
 class HBnBFacade:
     def __init__(self):
@@ -15,4 +20,20 @@ class HBnBFacade:
     # Placeholder method for fetching a place by ID
     def get_place(self, place_id):
         # Logic will be implemented in later tasks
+        pass
+
+    def create_amenity(self, amenity_data):
+    # Placeholder for logic to create an amenity
+        pass
+
+    def get_amenity(self, amenity_id):
+    # Placeholder for logic to retrieve an amenity by ID
+        pass
+
+    def get_all_amenities(self):
+    # Placeholder for logic to retrieve all amenities
+        pass
+
+    def update_amenity(self, amenity_id, amenity_data):
+    # Placeholder for logic to update an amenity
         pass
