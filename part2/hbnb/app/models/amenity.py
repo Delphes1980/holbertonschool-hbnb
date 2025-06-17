a1a4e20499b205c85187491caf9d568394bd12f1
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-06-17 14:19:04 +0200

    update the code to be at the same level as the branch Dev

--- part2/hbnb/app/models/amenity.py
+++ part2/hbnb/app/models/amenity.py
@@ -1,15 +1,16 @@
-from hbnb.app.models.baseEntity import BaseModel
+from hbnb.app.models.baseEntity import (BaseEntity, type_validation,
+                                        strlen_validation)
 
 
-class Amenity(BaseModel):
+class Amenity(BaseEntity):
     def __init__(self, name):
-        self.name = name
+        if not name:
+            raise ValueError("Name of the Amenity is required")
+        self.name = self.set_name(name)
 
-    def validate_name(name):
+    def set_name(self, name):
         """Verify if the name is a string < 50 characters."""
-        max_length = 50
-        if not isinstance(name, str):
-            raise TypeError("The name of the amenity must be a string")
-        if name > max_length:
-            raise ValueError(f"Name is more than {max_length} characters.")
+        type_validation(name, "Name of the Amenity", str)
+        name = name.strip()
+        strlen_validation(name, "Name of the Amenity", 1, 50)
         return name
