cf93ee49e3ee260a80fd104e19b9f412e5fbca4f
XavierLaforgue <xavierjimmys@gmail.com>
2025-06-30 13:45:15 +0200

    Update run.py and app/__init__.py
    Add configuration object to the Flask app and instantiate it with it (also remove input key-value 'debug=bool' so it is loaded from the config)

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -7,8 +7,9 @@ from app.api.v1.users import api as users_ns
 from app.api.v1.reviews import api as reviews_ns
 
 
-def create_app():
+def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
+    app.config.from_object(config_class)
     app.config['ERROR_INCLUDE_MESSAGE'] = False
     api = Api(app, version='1.0', title='HBnB API',
               description='HBnB Application API', doc='/api/v1/')
--- part3/hbnb/run.py
+++ part3/hbnb/run.py
@@ -1,10 +1,10 @@
 from app import create_app
 from flask import redirect
 
-app = create_app()
+app = create_app(config_class="config.DevelopmentConfig")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
 
 ##########################
 ########## TODO ##########
