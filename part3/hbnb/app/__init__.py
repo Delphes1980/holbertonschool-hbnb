758232c0d0c3654a291bb1c0dd3f6c98e737d7c6
XavierLaforgue <xavierjimmys@gmail.com>
2025-06-30 18:03:29 +0200

    app/__init__.py and requirements.txt update
    Install flask-jwt-extended, add to requirements,  and use it to instantiate a JWTmanager during app initialization

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -7,14 +7,17 @@ from app.api.v1.users import api as users_ns
 from app.api.v1.reviews import api as reviews_ns
 
 from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
 
 bcrypt = Bcrypt()
+jwt = JWTManager()
 
 def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
     app.config.from_object(config_class)
     app.config['ERROR_INCLUDE_MESSAGE'] = False
     bcrypt.init_app(app)
+    jwt.init_app(app)
     api = Api(app, version='1.0', title='HBnB API',
               description='HBnB Application API', doc='/api/v1/')
     
