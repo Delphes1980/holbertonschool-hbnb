64efc2a2931fa5fa3a9f60d5f3ab6b7ae0e03c0c
XavierLaforgue <xavierjimmys@gmail.com>
2025-07-02 17:51:30 +0200

    app/__init__.py update
    Change import instruction position to avoid circular import when importing bcrypt from app

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from flask_restx import Api
 
-from app.api.v1.amenities import api as amenities_ns
-from app.api.v1.places import api as places_ns
-from app.api.v1.users import api as users_ns
-from app.api.v1.reviews import api as reviews_ns
-from app.api.v1.auth import api as auth_ns
+# from app.api.v1.amenities import api as amenities_ns
+# from app.api.v1.places import api as places_ns
+# from app.api.v1.users import api as users_ns
+# from app.api.v1.reviews import api as reviews_ns
+# from app.api.v1.auth import api as auth_ns
 
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
@@ -31,6 +31,13 @@ def create_app(config_class="config.DevelopmentConfig"):
     # app.config['ERROR_INCLUDE_MESSAGE'] = False
     bcrypt.init_app(app)
     jwt.init_app(app)
+
+    from app.api.v1.amenities import api as amenities_ns
+    from app.api.v1.places import api as places_ns
+    from app.api.v1.users import api as users_ns
+    from app.api.v1.reviews import api as reviews_ns
+    from app.api.v1.auth import api as auth_ns
+
     api = Api(app, version='1.0', title='HBnB API',
               description='HBnB Application API', doc='/api/v1/',
               authorizations=authorizations)
