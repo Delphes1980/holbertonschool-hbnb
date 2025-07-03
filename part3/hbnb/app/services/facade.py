ab596df2e5fce4ad2d1602d1ca83fb3470427168
XavierLaforgue <xavierjimmys@gmail.com>
2025-07-04 01:32:20 +0200

    app/services/ReviewService.py and app/service/facade.py update
    Relegate review logic to ReviewService

--- part3/hbnb/app/services/facade.py
+++ part3/hbnb/app/services/facade.py
@@ -2,7 +2,7 @@ from app.persistence.repository import InMemoryRepository
 from app.services.PlaceService import PlaceService
 from app.services.UserService import UserService
 from app.services.AmenityService import AmenityService
-from app.models.review import Review
+from app.services.ReviewService import ReviewService
 from app.models.baseEntity import type_validation
 from app.services.ressources import is_valid_uuid4
 
@@ -78,76 +78,24 @@ class HBnBFacade:
 # Services for Review CRUD operations ########
 
     def create_review(self, review_data):
-        # Placeholder for logic to create a review, including
-        # validation for user_id, place_id, and rating
-        user_id = review_data.get('user_id')
-        if not user_id:
-            raise ValueError('Review data does not contain user_id key')
-        type_validation(user_id, 'user_id', str)
-        if not is_valid_uuid4(user_id):
-            raise ValueError('Given user_id is not valid UUID4')
-        place_id = review_data.get('place_id')
-        if not place_id:
-            raise ValueError('Review data does not contain place_id key')
-        if not is_valid_uuid4(place_id):
-            raise ValueError('Given place_id is not valid UUID4')
-        exisiting_user = self.user_repo.get(user_id)
-        if not exisiting_user:
-            raise ValueError('No User corresponding to given ID')
-        existing_place = self.place_repo.get(place_id)
-        if not existing_place:
-            raise ValueError('No Place corresponding to given place '
-                             'ID')
-        if self.get_review_by_place_and_user(place_id, user_id):
-            raise ValueError(
-                'User already left a review for this place')
-        review_data.pop('user_id')
-        review_data['user'] = exisiting_user
-        review_data.pop('place_id')
-        review_data['place'] = existing_place
-        review = Review(**review_data)
-        self.review_repo.add(review)
-        return review
+        return ReviewService.create_review(self, review_data)
 
     def get_review(self, review_id):
-        if not is_valid_uuid4(review_id):
-            raise ValueError('Given review_id is not valid UUID4')
-        return self.review_repo.get(review_id)
+        return ReviewService.get_review(self, review_id)
 
     def get_all_reviews(self):
         return self.review_repo.get_all()
 
     def get_reviews_by_place(self, place_id):
-        if not is_valid_uuid4(place_id):
-            raise ValueError('Given place_id is not valid UUID4')
-        place = self.get_place(place_id)
-        if not place:
-            return None
-        return place.reviews
+        return ReviewService.get_reviews_by_place(self, place_id)
 
     def get_review_by_place_and_user(self, place_id, user_id):
-        reviews = self.get_reviews_by_place(place_id)
-        if not reviews:
-            return None
-        for review in reviews:
-            if review.user.id == user_id:
-                return review
-        return None
+        return ReviewService.get_review_by_place_and_user(self,
+            place_id, user_id)
 
     def update_review(self, review_id, review_data):
-        review = self.get_review(review_id)
-        if not review:
-            return None
-        # self.review_repo.update(review_id, review_data)
-        review.update(review_data)
-        return review
+        return ReviewService.update_review(self, review_id,
+                                           review_data)
 
     def delete_review(self, review_id):
-        review = self.get_review(review_id)
-        if not review:
-            raise ValueError('Review not found')
-        for review_in_place in review.place.reviews:
-            if review_in_place.id == review.id:
-                review.place.reviews.remove(review)
-        self.review_repo.delete(review_id)
-        del review
+        ReviewService.delete_review(self, review_id)
