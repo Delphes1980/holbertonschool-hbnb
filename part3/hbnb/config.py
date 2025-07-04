c1c2bd5b4e857bb404a437a61b80ad00abb89ff1
XavierLaforgue <xavierjimmys@gmail.com>
2025-07-04 13:38:08 +0200

    config.py and app/__init__.py update
    Hardcode admin and regular users into the api developent configuration to facilitate testing before implementation of persistence

--- part3/hbnb/config.py
+++ part3/hbnb/config.py
@@ -3,11 +3,17 @@ import os
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
     DEBUG = False
+    ERROR_INCLUDE_MESSAGE = False
+    BCRYPT_LOG_ROUNDS = 12
 
 class DevelopmentConfig(Config):
     DEBUG = True
     ERROR_INCLUDE_MESSAGE = False
     BCRYPT_LOG_ROUNDS = 12
+    ADMIN_EMAIL = os.getenv('ADMIN_EMAIL', 'admin@example.com')
+    ADMIN_PASSWORD = os.getenv('ADMIN_PASSWORD', 'adminpassword')
+    REGULAR_USER_EMAIL = 'user@example.com'
+    REGULAR_USER_PASSWORD = 'password'
 
 config = {
     'development': DevelopmentConfig,
