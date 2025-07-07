afb98f7417fc70a46d9d76b2626d4528792b01aa
XavierLaforgue <xavierjimmys@gmail.com>
2025-07-07 12:24:34 +0200

    app/models/amenity.py update
    Map amenity class to database using sqlalchemy. Swap properties for hybrid_properties to validate inputs at creation and updates.

--- part3/hbnb/app/models/amenity.py
+++ part3/hbnb/app/models/amenity.py
@@ -1,21 +1,32 @@
-from app.models.baseEntity import (BaseEntity, type_validation,
+from .baseEntity import (BaseEntity, type_validation,
                                    strlen_validation)
-
+from app import db
+from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy.orm import mapped_column, Mapped
+from sqlalchemy import String
+from typing import Any
 
 class Amenity(BaseEntity):
+    __tablename__ = 'amenities'
+
+    _name: Mapped[str] = mapped_column("name", 
+                                       String(128), nullable=False, unique=True)
+
+
     def __init__(self, name):
         super().__init__()
         self.name = name
 
-    @property
-    def name(self):
-        return self.__name
+    @hybrid_property
+    def name(self) -> Any: # type: ignore
+        # Pylance may warn about 'name' being redefined (or obscured), but this is required for property setters.
+        return self._name # type: ignore
 
-    @name.setter
-    def name(self, value):
-        self.__name = self.name_validation(value)
+    @name.setter 
+    def name(self, value: str):
+        self._name = self.name_validation(value)
 
-    def name_validation(self, name: str):
+    def name_validation(self, name: str)->str:
         """Verify if the name is a string < 50 characters."""
         if name is None:
             raise ValueError('Invalid name: expected name but received None')
