ae6a076ebfd8aa9886af20a86d40df5573324166
XavierLaforgue <xavierjimmys@gmail.com>
2025-07-07 14:19:11 +0200

    config.py update
    Add database path and SQLAlchemy config

--- part3/hbnb/config.py
+++ part3/hbnb/config.py
@@ -10,12 +10,12 @@ class DevelopmentConfig(Config):
     DEBUG = True
     ERROR_INCLUDE_MESSAGE = False
     BCRYPT_LOG_ROUNDS = 12
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///development.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     ADMIN_EMAIL = os.getenv('ADMIN_EMAIL', 'admin@example.com')
     ADMIN_PASSWORD = os.getenv('ADMIN_PASSWORD', 'adminpassword')
     REGULAR_USER_EMAIL = 'user@example.com'
     REGULAR_USER_PASSWORD = 'password'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///development.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
     
 config = {
     'development': DevelopmentConfig,
