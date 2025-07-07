bfab6e3fed1e5639a5a6997d78438ec9d141b9f5
XavierLaforgue <xavierjimmys@gmail.com>
2025-07-07 14:24:39 +0200

    app/persistence/repository.py update
    Reinclude the InMemory repository for testing

--- part3/hbnb/app/persistence/repository.py
+++ part3/hbnb/app/persistence/repository.py
@@ -1,6 +1,5 @@
-from app import db
 from abc import ABC, abstractmethod
-
+from app import db
 
 class Repository(ABC):
     @abstractmethod
@@ -12,7 +11,7 @@ class Repository(ABC):
         pass
 
     @abstractmethod
-    def get_all(self):
+    def get_all(self) -> list:
         pass
 
     @abstractmethod
@@ -27,6 +26,30 @@ class Repository(ABC):
     def get_by_attribute(self, attr_name, attr_value):
         pass
 
+class InMemoryRepository(Repository):
+    def __init__(self):
+        self._storage = {}
+
+    def add(self, obj):
+        self._storage[obj.id] = obj
+
+    def get(self, obj_id):
+        return self._storage.get(obj_id)
+
+    def get_all(self):
+        return list(self._storage.values())
+
+    def update(self, obj_id, data):
+        obj = self.get(obj_id)
+        if obj:
+            obj.update(data)
+
+    def delete(self, obj_id):
+        if obj_id in self._storage:
+            del self._storage[obj_id]
+
+    def get_by_attribute(self, attr_name, attr_value):
+        return next((obj for obj in self._storage.values() if getattr(obj, attr_name) == attr_value), None)
 
 class SQLAlchemyRepository(Repository):
     def __init__(self, model):
@@ -45,8 +68,9 @@ class SQLAlchemyRepository(Repository):
     def update(self, obj_id, data):
         obj = self.get(obj_id)
         if obj:
-            for key, value in data.items():
-                setattr(obj, key, value)
+            # for key, value in data.items():
+            #     setattr(obj, key, value)
+            obj.update(data)
             db.session.commit()
 
     def delete(self, obj_id):
