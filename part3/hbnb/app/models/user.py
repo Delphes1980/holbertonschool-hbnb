da08708074c7b8ca275f589b079988b1b263071a
XavierLaforgue <xavierjimmys@gmail.com>
2025-07-07 12:40:51 +0200

    app/models/user.py update
    Map user to database. Swap properties to hybrid_properties to validate inputs

--- part3/hbnb/app/models/user.py
+++ part3/hbnb/app/models/user.py
@@ -1,11 +1,28 @@
-from app.models.baseEntity import (BaseEntity, type_validation,
+from .baseEntity import (BaseEntity, type_validation,
                                    strlen_validation)
 from validate_email_address import validate_email
 from app import bcrypt
 import re
+from sqlalchemy.orm import mapped_column, Mapped
+from sqlalchemy import String, Boolean
+from sqlalchemy.ext.hybrid import hybrid_property
 
 
 class User(BaseEntity):
+    __tablename__ = 'users'
+
+    _first_name: Mapped[str] = mapped_column("first_name",
+                                             String(50),
+                                             nullable=False)
+    _last_name: Mapped[str] = mapped_column("last_name",
+                                            String(50), nullable=False)
+    _email: Mapped[str] = mapped_column("email",
+                                        String(120), 
+                                        nullable=False, unique=True)
+    _password: Mapped[str] = mapped_column("password",
+                                           String(128), nullable=False)
+    _is_admin: Mapped[bool] = mapped_column("is_admin",
+                                            Boolean, default=False)
 
     def __init__(self, first_name: str, last_name: str,
                  email: str, password: str, is_admin: bool = False):
@@ -16,16 +33,16 @@ class User(BaseEntity):
         self.is_admin = is_admin
         self.password = password
 
-    @property
-    def password(self):
-        return self.password_hash
+    @hybrid_property
+    def password(self): # type: ignore
+        return self._password
     
     @password.setter
-    def password(self, password):
-        if password is None:
+    def password(self, value):
+        if value is None:
             raise ValueError('Expected password but received None')
-        type_validation(password, 'password', str)
-        self.password_hash = self.hash_password(password)
+        type_validation(value, 'password', str)
+        self._password = self.hash_password(value)
 
     def hash_password(self, password):
         """Hashes the password before storing it."""
@@ -65,38 +82,38 @@ class User(BaseEntity):
                              " example@exam.ple")
         return email
 
-    @property
-    def first_name(self):
-        return self.__first_name
+    @hybrid_property
+    def first_name(self): # type: ignore
+        return self._first_name
 
     @first_name.setter
-    def first_name(self, first_name):
-        self.__first_name = self.name_validation(first_name,
+    def first_name(self, value):
+        self._first_name = self.name_validation(value,
                                                  "first_name")
 
-    @property
-    def last_name(self):
-        return self.__last_name
+    @hybrid_property
+    def last_name(self): # type: ignore
+        return self._last_name
 
     @last_name.setter
-    def last_name(self, last_name):
-        self.__last_name = self.name_validation(last_name, "last_name")
+    def last_name(self, value):
+        self._last_name = self.name_validation(value, "last_name")
 
-    @property
-    def email(self):
-        return self.__email
+    @hybrid_property
+    def email(self): # type: ignore
+        return self._email
 
     @email.setter
-    def email(self, email: str):
-        self.__email = self.email_validation(email)
+    def email(self, value: str):
+        self._email = self.email_validation(value)
 
-    @property
-    def is_admin(self):
-        return self.__is_admin
+    @hybrid_property
+    def is_admin(self): # type: ignore
+        return self._is_admin
 
     @is_admin.setter
-    def is_admin(self, is_admin: bool):
-        if is_admin is None:
+    def is_admin(self, value: bool):
+        if value is None:
             raise ValueError('Expected boolean but received None')
-        type_validation(is_admin, "is_admin", bool)
-        self.__is_admin = is_admin
+        type_validation(value, "is_admin", bool)
+        self._is_admin = value
