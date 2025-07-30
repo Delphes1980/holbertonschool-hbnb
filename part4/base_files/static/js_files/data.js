137eefb95fc3827c9b7d1cc3b255b9194071a22b
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-07-30 18:29:13 +0200

    delete the database before connexion to the API

--- part4/base_files/static/js_files/data.js
+++
@@ -1,6 +0,0 @@
-const placeData = [
-    { id: 1, name: 'Cute little house', price: 25, owner: 'Charles Ingalls', description: 'Cute little house in the prairy. A beautiful place where you can find yourself.. or nobody.', amenities: 'None', imageUrl: '../images/test_maison1.jpg' },
-    { id: 2, name: 'Modern house', price: 150, owner: 'Big Daddy', description: 'Big modern house with all the comfort you need.', amenities: 'Swimming pool, WiFi, Electricity', imageUrl: '../images/test_maison2.jpg' },
-    { id: 3, name: 'Cozy Cottage', price: 75, owner: 'Michael Myers', description: 'A charming cottage by the lake', amenities: 'Fireplace, Lake view', imageUrl: '../images/test_maison3.jpg' },
-    { id: 4, name: 'City Apartment', price: 100, owner: 'Carrie Bradshaw', description: 'Spacious apartment near the beach', amenities: 'Balcony, Beach view', imageUrl: '../images/test_maison4.jpg' }
-];
